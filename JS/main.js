0ca918b4b6f61ed39768180d6f794dca39ee53a3
Kareem zakaria <kareemzakaria.business@gmail.com>
2024-07-11 22:45:23 +0300

    added background

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+//  document.querySelector(".toggle-settings .fa-sharp.fa-solid.fa-bars").onclick = function () {
+//     //Toggle Class Open On Main Settings Box
+//     document.querySelector(".settings-box").classList.toggle("open");
+// };
